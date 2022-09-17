2822f48daa1da457801db8f9a9b1be6f5350566f
Abhijit <abhijitthorat1991@gmail.com>
2022-09-17 19:16:48 +0530

    Added changes with Repository pattern

--- BettingAPI/Controllers/SportsController.cs
+++ BettingAPI/Controllers/SportsController.cs
@@ -1,4 +1,5 @@
-﻿using Core.Models;
+﻿using BettingAPI.Repository.IRepository;
+using Core.Models;
 using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json.Linq;
 
@@ -8,10 +9,12 @@ namespace BettingAPI.Controllers
     [Route("[controller]")]
     public class SportsController : ControllerBase
     {
-        //public IActionResult Index()
-        //{
-        //    return View();
-        //}
+        IRepository<Sport> _sportsRepository;
+        
+        public SportsController(IRepository<Sport> sportRepo)
+        {
+            _sportsRepository = sportRepo;
+        }
 
         [HttpGet]
         public IActionResult GetSports()
@@ -19,44 +22,9 @@ namespace BettingAPI.Controllers
             var jspnFilePath = "C:\\Users\\HP\\OneDrive\\Desktop\\JsonFiles\\sports#languageCode_en.json";
             List<Sport> Sports = new List<Sport>();
 
-            using (StreamReader r = new StreamReader(jspnFilePath))
-            {
-                string json = r.ReadToEnd();
-                var sportsList = JObject.Parse(json)["sports"].ToList();
-                IList<Sport> lstSports = new List<Sport>();
-
-                foreach (var item in sportsList)
-                {
-                    lstSports.Add(item.ToObject<Sport>());
-                }
-                Sports = lstSports.ToList();
-            }
+            Sports = _sportsRepository.GetData(jspnFilePath);
 
             return Ok(Sports);
-        }
-
-        [HttpGet("{sportId:int}")]
-        public IActionResult GetTournaments(int sportId)
-        {
-            var jspnFilePath = "C:\\Users\\HP\\OneDrive\\Desktop\\JsonFiles\\sportstournaments#countryIds_181&languageCode_en.json";
-            List<Tournament> tournaments = new List<Tournament>();
-
-            using (StreamReader r = new StreamReader(jspnFilePath))
-            {
-                string json = r.ReadToEnd();
-                var tournamentsList = JObject.Parse(json)["tournaments"].ToList();
-                IList<Tournament> lstTournaments = new List<Tournament>();
-
-                foreach (var item in tournamentsList)
-                {
-                    lstTournaments.Add(item.ToObject<Tournament>());
-                }
-
-                tournaments = lstTournaments.Where(x => x.sportId == sportId).ToList();
-
-            }
-
-            return Ok(tournaments);
         }
     }
 }
