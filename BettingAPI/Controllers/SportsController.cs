ae178daa4a98420f6aede8e728b80cb2b75a8954
Abhijit <abhijitthorat1991@gmail.com>
2022-09-17 16:53:37 +0530

    Added Sports Controller

---
+++ BettingAPI/Controllers/SportsController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BettingAPI.Controllers
+{
+    public class SportsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        
+    }
+}
