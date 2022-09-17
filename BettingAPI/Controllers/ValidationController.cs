82e9e8c3939a980b35b11e587f9c76e9e5783c33
LENOVO <LENOVO@DESKTOP-R3L0K5C>
2022-09-17 16:47:01 +0530

    Initial Changes

---
+++ BettingAPI/Controllers/ValidationController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BettingAPI.Controllers
+{
+    public class ValidationController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
